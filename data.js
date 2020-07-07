01bd7247db468c2b4c82e35d6fe8e1afad3346be
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-07-07 12:16:58 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+/* IT 122 javascript - 2
+node.js with 5 array 4 attributes
+Prepared: Omar 
+*/
+
+let students = [
+{ name : 'Omar', age : 40, classes : ['Hum105', 'Eng101', 'Itc225'] },
+{ name : 'Ali', age : 30, classes : ['ITC122', 'web120', 'Eng 102'] },
+{ name : 'Mohamud', age : 20, classes : ['Itc229', 'Itc155', 'web120'] },
+{ name : 'Ramlo', age : 33, classes : ['Itc172', 'ENG235', 'web120'] },
+{ name : 'Muna', age : 25, classes : ['web150', 'ENG235', 'web110'] }
+];
+
+exports.getAll = () => {
+    return student;
+}
+
