97ec7abf84f53af04ad8ae3ab88239f0e58b28da
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-07-09 17:52:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // this is sample file
 const http = require("http");
+const students = require("http");
+
+
+/*creating a var to get all the data from students*/
+let showstudents = students.getAll();
+
 
 http.createServer(
     (req,res) => {
@@ -7,11 +13,11 @@ http.createServer(
         switch(path) {
             case '/':
                 res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end('Home page');
+                res.end('Home page\n' + 'arraylenght:'+ showstudents.length);
                 break;
             case '/about':
                 res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end('About page');
+                res.end('About page \n This is Omar Garmela, \n I\'am doing my programming degree.I probably, i will finish the following winter');
                 break;
             default:
                 res.writeHead(404, {'Content-Type': 'text/plain'});
