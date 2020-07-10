1af983783e542174868144d96e25550393cdf9a6
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-07-09 19:08:35 -0700

    Update data.js

--- data.js
+++ data.js
@@ -4,13 +4,14 @@ Prepared: Omar
 */
 
 let students = [
-{ name : 'Omar', age : 40, classes : ['Hum105', 'Eng101', 'Itc225'] },
-{ name : 'Ali', age : 30, classes : ['ITC122', 'web120', 'Eng 102'] },
-{ name : 'Mohamud', age : 20, classes : ['Itc229', 'Itc155', 'web120'] },
-{ name : 'Ramlo', age : 33, classes : ['Itc172', 'ENG235', 'web120'] },
-{ name : 'Muna', age : 25, classes : ['web150', 'ENG235', 'web110'] }
+{ name : 'Omar', age : 40, classes : 'Hum105', 'Eng101', 'Itc225'},
+{ name : 'Ali', age : 30, classes : 'ITC122', 'web120', 'Eng 102'},
+{ name : 'Mohamud', age : 20, classes : 'Itc229', 'Itc155', 'web120'},
+{ name : 'Ramlo', age : 33, classes : 'Itc172', 'ENG235', 'web120'},
+{ name : 'Muna', age : 25, classes : 'web150', 'ENG235', 'web110'}
 ];
 
+// an exported getAll method that returns all array items
 exports.getAll = () => {
     return student;
 }
