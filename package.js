9d6d5f5bc596d55defee2a61b6250218a5c6a889
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-07-09 19:13:38 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,7 @@
+{
+"program":"students data",
+"discription": "data students in modules array",
+"main":"index.js",
+"repository":"github",
+"github":"https://github.com/sebastianquintana/ITC-122"
+}
