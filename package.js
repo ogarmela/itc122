06d4777e8db14664bfa074246e4ebdc7af90ef52
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-07-18 16:56:42 -0700

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,24 @@
 {
-"program":"students data",
-"discription": "data students in modules array",
-"main":"index.js",
-"repository":"github",
-"https://github.com/ogarmela/ITC122_JSON_1/tree/master"
+  "github": "https://github.com/ogarmela/itc122",
+  "dependencies": {
+    "body-parser": "^1.19.0",
+    "express": "^4.17.1",
+    "express-handlebars": "^5.0.0",
+    "nodemon": "^2.0.4"
+  },
+  "name": "itc-122",
+  "description": "",
+  "version": "1.0.0",
+  "main": "index.js",
+  "devDependencies": {
+    "babel-cli": "^6.26.0",
+    "babel-preset-env": "^1.7.0",
+    "babel-preset-stage-0": "^6.24.1",
+    "eslint": "^7.4.0"
+  },
+  "scripts": {
+    "start": "nodemon ./index.js --exec babel-node -e js"
+  },
+  "author": "Omar Garmela",
 }
+
