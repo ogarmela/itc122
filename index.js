5da371c5a46f652160744c26baea544deea0eed7
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-07-18 17:21:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-/*creating variable to get modules from data.js*/
+'use strict'
+const express = require("express");
+const bodyParser= require("body-parser");
+const music = require("./data");
+/*var routes = require('./routes.js')(app); // pass ‘app’ instance to the routes module*/
 
-const http = require("http");
-const students = require("./data");
 
+
+const app= express();
+
+
+app.set('port',process.env.PORT || 3000);
+app.use(express.static('index'));
+app.use('/css',express.static('css')); // set location for static files
+app.use(bodyParser.urlencoded({extended: true})); // parse form submissions)
+
+let show = require("express-handlebars"); // should be at top of module 
+
+app.engine('handlebars', students({defaultLayout: false}));
+app.set("view engine", "handlebars");
 
 /*creating a var to get all the data from students*/
 let showstudents = students.getAll();
 
+// send static file as response
+app.get('/', (req, res) => {
+    res.type('text/html');
+    res.sendFile('home',{students:showstudents}); 
+   });
+   
+app.get('/detail', (req, res) => {
+    const students.courses = req.query.courses
+    res.render('detail', {courses: studentscourses, stats: students.getDetail(studentcourses)});
+});
 
-http.createServer(
-    (req,res) => {
-        const path = req.url.toLowerCase();
-        switch(path) {
-            case '/':
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end('Home page\n' + 'arraylenght:'+ showstudents.length);
-                break;
-                
-            case '/about':
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end('About page \n This is Omar Garmela, \n I\'am doing my programming degree.I probably, i will finish the following winter');
-                break;
-            default:
-                res.writeHead(404, {'Content-Type': 'text/plain'});
-                res.end('Not found');
-                break;
-        }
-    }).listen(process.env.PORT || 3000);
-
-
-
-
+app.get('/About', (req, res)=>{
+    res.type('text/plain');
+    res.send('About page \n This is Omar Garmela, \n I\'am doing my programming degree.I probably, i will finish the following winter');
+    
+    // define 404 handler
+   app.use( (req,res) => {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Not found');
+   });
+   app.courses(app.get('PORT'),()=>{
+       console.log('express started now')
+   });
