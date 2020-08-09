9bcabebd493c4130ece15aea190de741c1298447
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-08-08 20:05:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,58 @@
-'use strict'
+"use strict"
+
+let book = require("../lib/book.js");
+
 const express = require("express");
-const bodyParser= require("body-parser");
-const music = require("./data");
-/*var routes = require('./routes.js')(app); // pass ‘app’ instance to the routes module*/
+const app = express();
 
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // allows direct navigation to static files
+app.use(require("body-parser").urlencoded({extended: true}));
 
+const exphbs =  require("express-handlebars");
+app.engine('hbs', exphbs({defaultLayout: false}));
+app.set("view engine", "hbs");
 
-const app= express();
+app.get('/', (req,res) => {
+    res.render('home', {courses: course.getAll()}); 
+});
 
-
-app.set('port',process.env.PORT || 3000);
-app.use(express.static('index'));
-app.use('/css',express.static('css')); // set location for static files
-app.use(bodyParser.urlencoded({extended: true})); // parse form submissions)
-
-let show = require("express-handlebars"); // should be at top of module 
-
-app.engine('handlebars', students({defaultLayout: false}));
-app.set("view engine", "handlebars");
-
-/*creating a var to get all the data from students*/
-let showstudents = students.getAll();
-
-// send static file as response
-app.get('/', (req,res, next) => {
-    course.find((err,courses) => {
-        console.log(coursess)
-        if (err) return next(err);
-        res.render('home', {courses: JSON.stringify(courses)});
-   });
-   
-app.get('/delete', (req, res) => {
-    const coursesno = req.query.no;
-    courses.findByIDAndDelete({title: courses}, (err, courses) => {
-        
-        if (err) {
-            console.log(err);
-        } else if (!course) {
-            console.log(courses + "course not found");
-            res.send(`${courseno} not found`);
-        } else if (movie) {
-            console.log(courseno + "Removed");
-            res.send(`${coursesno} Removed`);
-        }
-    });
-
-app.get('/About', (req, res)=>{
+// send plain text response
+app.get('/about', function(req,res){
     res.type('text/plain');
-    res.render('About page \n This is Omar Garmela, \n I\'am doing my programming degree.I probably, i will finish the following winter');
-    
-    // define 404 handler
-   app.use( (req,res) => {
+    res.send('About page');
+});
+
+// handle GET 
+app.get('/delete', function(req,res){
+    let result = course.delete(req.query.no); // delete course object
+    res.render('delete', {no: req.query.no, result: result});
+});
+
+app.get('/detail', function(req,res){
+    console.log(req.query)
+    var found = course.get(req.query.no);
+    res.render("details", {
+        no: req.query.no, 
+        result: found
+        }
+    );
+});
+
+// handle POST
+app.post('/detail', function(req,res){
+    console.log(req.body)
+    var found = course.get(req.body.no);
+    res.render("details", {no: req.body.no, result: found, courses: courses.getAll()});
+});
+
+// define 404 handler
+app.use(function(req,res) {
     res.type('text/plain'); 
     res.status(404);
     res.send('404 - Not found');
-   });
-   app.courses(app.get('PORT'),()=>{
-       console.log('express started now')
-   });
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started');    
+});
