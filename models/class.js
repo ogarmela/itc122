52b1115870d40770485729ff1f2a6b78ff16924d
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-08-20 16:49:40 -0700

    Update and rename Class.js to class.js

--- models/class.js
+++ models/class.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const credentials = require("../credentials");
+const credentials = require("./credentials");
 // local db connection settings 
 // const ip = process.env.ip || '127.0.0.1';
 // const connectionString = 'mongodb://' +ip+ '/<DB_NAME>';
 
-mongoose.connect(credentials.connectionString, { dbName: "movies.project", useNewUrlParser: true }); 
+mongoose.connect(credentials.connectionString, { dbName: "class.project", useNewUrlParser: true }); 
 mongoose.set('useFindAndModify', false);
 mongoose.connection.on('open', () => {
   console.log('Mongoose connected.');
@@ -13,10 +13,10 @@ mongoose.connection.on('open', () => {
 // define Book model in JSON key/value pairs
 // values indicate the data type of each key
 const mySchema = mongoose.Schema({
-  title: { type: String, required: true },
-  director: String,
+  clasno: { type: String, required: true },
+  student: String,
   type:String,
   year: Number
  }); 
  
- module.exports = mongoose.model('Movie', mySchema,"Movies");
+ module.exports = mongoose.model('class', mySchema,"class");
