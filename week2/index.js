00a339688cba887e7dce6c73f003148d083d4427
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-09-26 10:57:15 -0700

    Create index.js

---
+++ week2/index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+let movies = require("./lib/module.js");
+
+const express = require("express");
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // allows direct navigation to static files
+app.use(require("body-parser").urlencoded({extended: true}));
+
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
+
+// send static file as response
+app.get('/', function(req,res){
+    res.type('text/html');
+    res.sendFile(__dirname + '/public/home.html'); 
+});
+
+// send plain text response
+app.get('/about', function(req,res){
+    res.type('text/plain');
+    res.send('About page');
+});
+
+// handle GET 
+app.get('/delete', function(req,res){
+    let result = movies.delete(req.query.title); // delete movies object
+    res.render('delete', {title: req.query.title, result: result});
+});
+
+// handle POST
+app.post('/get', function(req,res){
+    console.log(req.body)
+    var header = 'Searching for: ' + req.body.title + '<br>';
+    var found = movies.get(req.body.title);
+    res.render("details", {title: req.body.title, result: found});
+});
+
+// define 404 handler
+app.use(function(req,res) {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started');    
+});
