6d203970df2a31acff649dca8ac07dea2e2ec714
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-12-13 19:15:51 -0800

    Create book.js

---
+++ week6/lib/book.js
@@ -0,0 +1,14 @@
+var Book = require("../models/book.js");
+
+// This prints out aLL contents of this database
+exports.getAll = () => {
+  return Book.find({}, (err, result) => {
+    if (err) {
+      return err;
+    }
+    // line below will print out number of items in database
+    console.log(result.length);
+    // LINE BELOW will render result to web-client-page
+    return result;
+  });
+};
